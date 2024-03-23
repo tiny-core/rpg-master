1155a54cc476e2521778ab16822a7269a401e91a
Jocian de Souza Mendonça <jocianmendonca@gmail.com>
2024-03-23 18:37:16 +0000

    [player] added the player package

---
+++ packages/player/app.vue
@@ -0,0 +1,20 @@
+<template>
+	<v-app>
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+	</v-app>
+</template>
+
+<style lang="scss">
+body,
+html {
+	overflow: hidden;
+}
+
+.bg-image {
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+}
+</style>
---
+++ packages/player/layouts/blank.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+	<v-main app>
+		<slot />
+	</v-main>
+</template>
+
+<style scoped></style>
