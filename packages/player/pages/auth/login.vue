145d5acd0f6d1fc30539e5511762ddbf817697cc
Jocian de Souza Mendonça <jocianmendonca@gmail.com>
2024-03-23 21:04:23 +0000

    [player] added nuxt firebase and a basic login page

---
+++ packages/player/pages/auth/login.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+import { GoogleAuthProvider } from 'firebase/auth'
+export const googleAuthProvider = new GoogleAuthProvider()
+</script>
+
+<script lang="ts" setup>
+import { signInWithPopup } from 'firebase/auth'
+
+const auth = useFirebaseAuth()
+const router = useRouter()
+const error = ref(null)
+
+function signIn() {
+	error.value = null
+
+	if (auth)
+		signInWithPopup(auth, googleAuthProvider)
+			.then(() => router.push('/'))
+			.catch((reason) => {
+				console.error('Failed sign', reason)
+				error.value = reason
+			})
+}
+
+onMounted(() => signIn())
+</script>
+
+<template>
+	<v-container class="fill-height">
+		<v-row justify="center" align="center">
+			<v-col v-if="error" cols="12" class="text-center">
+				<div>{{ error }}</div>
+				<v-divider class="my-3" />
+				<v-btn prepend-icon="fab fa-google" @click="signIn">Login</v-btn>
+			</v-col>
+			<v-col v-else class="text-center">
+				<v-progress-circular size="60" indeterminate />
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<style scoped></style>
